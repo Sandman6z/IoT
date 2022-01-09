a08d040fe138fd7b33d2016f98627e580ea2e11b
Sandman006 <zbn0238@163.com>
2022-01-09 16:24:41 +0800

    change some commit

--- ESP12_DS18B20/ESP12_DS18B20.ino
+++ ESP12_DS18B20/ESP12_DS18B20.ino
@@ -15,11 +15,10 @@ const char* password = "woaisfair";
 
 ESP8266WebServer server(80);
 
-float tempC;  // Values read from sensor
-String webString = "";     // String to display
+float tempC;            // Values read from sensor
+String webString = "";  // String to display
 
- 
-void handle_root() 
+void handle_root()
 {
   server.send(200, "text/plain", "Hello from the weather esp8266, read from /temp or /humidity");
   delay(100);
@@ -27,63 +26,56 @@ void handle_root()
 
 void setup(void)
 {
-  Serial.begin(115200);  
+  Serial.begin(115200);
   WiFi.begin(ssid, password);
   Serial.print("\n\r \n\rWorking to connect");
 
   // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) 
+  while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
   }
-  
+
   Serial.print("Locating devices...");
   sensors.begin();
   Serial.print("Found ");
   Serial.print(sensors.getDeviceCount(), DEC);
   Serial.println(" devices.");
-  
+
   Serial.println("");
-  
+
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
- // report parasite power requirements
-  Serial.print("Parasite power is: "); 
-  if (sensors.isParasitePowerMode()) 
+
+  Serial.print("Parasite power is: ");  // report parasite power requirements
+  if (sensors.isParasitePowerMode())
     Serial.println("ON");
-  else 
+  else
     Serial.println("OFF");
-  
+
   if (!sensors.getAddress(insideThermometer, 0))
-    Serial.println("Unable to find address for Device 0"); 
+    Serial.println("Unable to find address for Device 0");
 
-
-// show the addresses we found on the bus
-  Serial.print("Device 0 Address: ");
+  Serial.print("Device 0 Address: ");   // show the addresses we found on the bus
   printAddress(insideThermometer);
   Serial.println();
 
   // set the resolution to 9 bit (Each Dallas/Maxim device is capable of several different resolutions)
   sensors.setResolution(insideThermometer, 9);
- 
+
   Serial.print("Device 0 Resolution: ");
-  Serial.print(sensors.getResolution(insideThermometer), DEC); 
+  Serial.print(sensors.getResolution(insideThermometer), DEC);
   Serial.println();
 
-  
-    
-   //初始化WebServer
-  server.on("/", handle_root);
-  
-  server.on("/temp", [](){
-    webString="Temperature: "+String(tempC)+ "C";   // Arduino has a hard time with float to string
+  server.on("/", handle_root);    //初始化WebServer
+  server.on("/temp", []() {
+    webString = "Temperature: " + String(tempC) + "C"; // Arduino has a hard time with float to string
     server.send(200, "text/plain", webString);            // send to someones browser when asked
   });
-  
   server.begin();
   Serial.println("HTTP server started");
 }
@@ -92,7 +84,7 @@ void setup(void)
 void printTemperature(DeviceAddress deviceAddress)
 {
   tempC = sensors.getTempC(deviceAddress);
-  if(tempC == DEVICE_DISCONNECTED_C) 
+  if (tempC == DEVICE_DISCONNECTED_C)
   {
     Serial.println("Error: Could not read temperature data");
     return;
@@ -101,24 +93,18 @@ void printTemperature(DeviceAddress deviceAddress)
   Serial.println(tempC);
 }
 
-
-
-
 void loop(void)
 {
   Serial.print("Requesting temperatures...");
   sensors.requestTemperatures(); // Send the command to get temperatures
   Serial.println("DONE");
-  
-  // It responds almost immediately. Let's print out the data
+
   printTemperature(insideThermometer); // Use a simple function to print out the data
-  
+
   server.handleClient();
-} 
+}
 
-
-// function to print a device address
-void printAddress(DeviceAddress deviceAddress)
+void printAddress(DeviceAddress deviceAddress)  // function to print a device address
 {
   for (uint8_t i = 0; i < 8; i++)
   {
